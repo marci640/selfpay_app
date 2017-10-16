1192fd82b01f9b690f8bab416d370905785c5c26
marci <marcifey@gmail.com>
2017-10-16 09:34:26 -0700

    added content to letter from form

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -1,6 +1,16 @@
 class LettersController < ApplicationController
 
   def create
-    
+    @codes = []
+    index = 1
+    8.times do 
+      if params[:"table_row#{index}"]
+        @codes << params[:"table_row#{index}"]
+      end
+    end
   end 
+    
+    
+
+ 
 end
